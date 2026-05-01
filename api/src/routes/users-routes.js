c3ef7b412ecf3470d9a13f0db44740cc9b802bb3
Thiago <thiagomarcelino897@gmail.com>
2026-05-01 09:18:44 -0300

    chore: clean up compiled js files from src and configure outDir

--- api/src/routes/users-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usersRoutes = void 0;
-const express_1 = require("express");
-const users_controller_1 = require("@/controllers/users-controller");
-const usersRoutes = (0, express_1.Router)();
-exports.usersRoutes = usersRoutes;
-const usersController = new users_controller_1.UsersController();
-usersRoutes.post("/", usersController.create);
